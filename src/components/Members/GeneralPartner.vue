d0caaed65d3eae76c5d6dd56e0b0232edba9562c
Nikita Antonov <nikita45454@gmail.com>
2025-03-24 22:48:36 +0500

    feat: заполняет ген партнера

--- src/components/Members/GeneralPartner.vue
+++ src/components/Members/GeneralPartner.vue
@@ -1,18 +1,37 @@
 <template>
   <section class="section-gen-partner">
     <h3 class="section-gen-partner__title">Генеральный партнер</h3>
-    <article class="item">
+    <article class="item" @click="showed = true">
       <div class="photo">
-        <img class="photo__img" width="320" height="320" src="@/assets/images/general-partner/preview.jpg" loading="lazy" />
+        <img class="photo__img" width="320" height="320" :src="item.preview" loading="lazy" />
       </div>
-      <h4 class="title">Kulikov</h4>
-      <p class="text">Сеть кондитерских</p>
+      <h4 class="title">{{ item.name }}</h4>
+      <p class="text">{{ item.description }}</p>
     </article>
+    <ModalShow v-model="showed" :item="item" />
   </section>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+  import ModalShow from './ModalShow.vue';
 
+  const showed = ref(false);
+
+  const item = {
+    name: 'Kulikov',
+    description: 'Сеть кондитерских',
+    preview: '/members/kulikov/preview.jpg',
+    text: `
+      Kulikov — это волшебство праздника, заключенное в каждом десерте. Мы создаем уникальные моменты радости и наслаждения, используя только натуральные ингредиенты в наших кондитерских изделиях ручной работы. Каждый торт, пирожное или сладость наполняет атмосферу теплом и счастьем.<br/><br/>Компания Kulikov уже более 33 лет, радует своих гостей сладостями.<br/>- в нашем ассортименте более 200 продуктов, чтобы каждый нашел свое. Мы каждый день стремимся стать лучше для наших любимых гостей и с трепетом относимся к каждому отзыву. Мы черпаем вдохновение в улыбках потребителей и постоянно расширяем ассортимент, пробуем новое и следуем трендам.
+    `,
+    caption: 'Раздача торта гиганта: 6 апреля с 13:00 до 13:40',
+    logo: {
+      width: '154',
+      height: '100',
+      src: '/members/kulikov/logo.png',
+    }
+  };
 </script>
 
 <style scoped lang="scss">
@@ -28,9 +47,24 @@
   }
 
   .item {
+    cursor: pointer;
     max-width: 320px;
     margin: 0 auto;
     text-align: center;
+
+    &:hover {
+      .photo__img {
+        transform: scale(1.05);
+      }
+
+      .title {
+        color: var(--color-primary);
+      }
+
+      .text {
+        opacity: 0.7;
+      }
+    }
   }
 
   .photo {
@@ -39,6 +73,7 @@
     margin-bottom: 12px;
 
     &__img {
+      transition: transform 150ms;
       width: 100%;
     }
   }
