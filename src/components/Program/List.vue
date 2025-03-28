f46112604e6f70738bdfba93f136a83cffa12ca0
Nikita Antonov <nikita45454@gmail.com>
2025-03-28 12:40:23 +0500

    feat: program - добавляет пагинатор

--- src/components/Program/List.vue
+++ src/components/Program/List.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="prog-items" ref="itemsRef">
-    <ListItem
-      class="prog-item"
-      v-for="item in items"
-      :item="item"
-    />
+  <div class="prog-list">
+    <div class="prog-items" ref="itemsRef">
+      <ListItem
+        class="prog-item"
+        v-for="item in data"
+        :item="item"
+      />
+    </div>
+    <BaseButton v-if="!grid.md && !isEnd" class="prog-list__btn" @click="showMore">
+      Показать еще
+    </BaseButton>
   </div>
 </template>
 
 <script setup>
-  import { onMounted, ref } from 'vue';
+  import { computed, onMounted, reactive, ref } from 'vue';
   import ListItem from './ListItem.vue';
   import useAppGrid from '@/composables/useAppGrid';
+  import BaseButton from '@/components/Base/Button.vue';
 
-  defineProps({
+  const props = defineProps({
     items: {
       required: true,
       type: Array,
@@ -58,9 +64,39 @@
     itemsRef.value.style.setProperty('--textMaxHeight', `${textMaxHeight}px`);
     itemsRef.value.style.setProperty('--datesMaxHeight', `${datesMaxHeight}px`);
   }
+
+  const paginator = reactive({
+    step: 4,
+    limit: Math.min(4, props.items.length),
+  });
+
+  const data = computed(() => {
+    if(grid.md) return props.items;
+    return props.items.slice(0, paginator.limit);
+  });
+
+  function showMore() {
+    paginator.limit = Math.min(paginator.limit + paginator.step, props.items.length);
+  }
+
+  const isEnd = computed(() => {
+    return paginator.limit >= props.items.length;
+  });
 </script>
 
 <style scoped lang="scss">
+  .prog-list {
+    &__btn {
+      display: block;
+      max-width: 220px;
+      width: 100%;
+      margin: 0 auto;
+      margin-top: 32px;
+      text-transform: uppercase;
+      font-size: 14px;
+    }
+  }
+
   .prog-items {
     display: flex;
     flex-wrap: wrap;
